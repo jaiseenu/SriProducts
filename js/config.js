7d0e51605f64ef8f75db78ec4eb8d333dbf0acec
Jaiseenu V <72805814+jaiseenu@users.noreply.github.com>
2026-09-04 12:30:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Edit this one value after you deploy the Apps Script Web App.
 // Deploy > New deployment > Web app > copy the URL ending in /exec.
 window.SRI_CONFIG = {
-  API_BASE_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE'
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx5T-ZMPY51uRBX2lNxDUVVBJLLOmzY0YdbSE2IIxlMIaHZOVGfAlJq8cnknyiPsiMu/exec'
 };
